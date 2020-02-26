3fd16699029c0a42737102b2b9d7d693a34c6c46
1338 <maxvandersluis@yahoo.com>
2020-02-26 15:29:36 +0100

    node-js build

--- scripts/createrom.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd /var/lib/tftpboot/ipxe/ipxe-git/src/
-make bin/undionly.kpxe EMBED=/var/lib/tftpboot/ipxe/script.ipxe > /dev/null 2>&1
-cp /var/lib/tftpboot/ipxe/ipxe-git/src/bin/undionly.kpxe /var/lib/tftpboot/
--- web/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require('config.php');
-
-$stmt = Database::run("SELECT * FROM table")->fetch();
-
-
-?>
